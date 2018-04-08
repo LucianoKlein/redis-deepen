d5bb1afbebe9c91528dd41d44c158601db1af6fc
LucianoKlein <kleinersilver@gmail.com>
2018-04-08 19:14:15 +0800

    finish

--- src/test/java/com/hexin/redis/TestPool.java
+++ src/test/java/com/hexin/redis/TestPool.java
@@ -6,8 +6,6 @@ import redis.clients.jedis.JedisPool;
 public class TestPool {
     public static void main(String[] args) {
         JedisPool jedisPool = JedisPoolUtil.getJedisPoolInstance();
-        JedisPool jedisPool2 = JedisPoolUtil.getJedisPoolInstance();
-        System.out.println(jedisPool == jedisPool2);
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
