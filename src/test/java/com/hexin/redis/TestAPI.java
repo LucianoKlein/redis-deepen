3e54c48375331b42ca79e73faee220cc1a026b80
LucianoKlein <kleinersilver@gmail.com>
2018-04-08 12:02:19 +0800

    key-value

--- src/test/java/com/hexin/redis/TestAPI.java
+++ src/test/java/com/hexin/redis/TestAPI.java
@@ -2,11 +2,15 @@ package com.hexin.redis;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Set;
+
 public class TestAPI {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("127.0.0.1", 6379);
 
         System.out.println(jedis.get("k5"));
-        System.out.println();
+        System.out.println(jedis.keys("*"));
+        Set<String> set = jedis.keys("*");
+        System.out.println(set.size());
     }
 }
