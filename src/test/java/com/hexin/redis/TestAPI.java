8e2a900219f44e046968608e13899c940e8e046c
LucianoKlein <kleinersilver@gmail.com>
2018-04-08 12:27:34 +0800

    set-jedis

--- src/test/java/com/hexin/redis/TestAPI.java
+++ src/test/java/com/hexin/redis/TestAPI.java
@@ -2,15 +2,30 @@ package com.hexin.redis;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class TestAPI {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("127.0.0.1", 6379);
 
-        System.out.println(jedis.get("k5"));
-        System.out.println(jedis.keys("*"));
-        Set<String> set = jedis.keys("*");
-        System.out.println(set.size());
+        jedis.sadd("orders", "jd001");
+        jedis.sadd("orders", "jd002");
+        jedis.sadd("orders", "jd003");
+
+        Set<String> set1 = jedis.smembers("orders");
+        for (Iterator iterator = set1.iterator(); iterator.hasNext();) {
+           String string = (String) iterator.next();
+           System.out.println(string);
+        }
+
+        jedis.srem("orders", "jd002");
+
+        set1 = jedis.smembers("orders");
+        for (Iterator iterator = set1.iterator(); iterator.hasNext();) {
+            String string = (String) iterator.next();
+            System.out.println(string);
+        }
     }
 }
