57bbcfcca8af718abe70edb877c22314ac848043
LucianoKlein <kleinersilver@gmail.com>
2018-04-08 16:18:41 +0800

    transaction test

--- src/test/java/com/hexin/redis/TestTransaction.java
+++ src/test/java/com/hexin/redis/TestTransaction.java
@@ -4,15 +4,40 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Transaction;
 
 public class TestTransaction {
-    public static void main(String[] args) {
-        Jedis jedis = new Jedis("localhost", 6379);
-        Transaction transaction = jedis.multi();
+    public boolean transMethod() throws InterruptedException {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        int balance;// 可用余额
+        int debt;// 欠额
+        int amtToSubtract = 10;// 实刷额度
 
-        transaction.set("k4", "v44");
-        transaction.set("k5", "v55");
+        jedis.watch("balance");
 
-        transaction.discard();
-        System.out.println(jedis.get("k4"));
-        System.out.println(jedis.get("k5"));
+        //jedis.set("balance","5");//此句不该出现，讲课方便。模拟其他程序已经修改了该条目
+        Thread.sleep(7000);
+        balance = Integer.parseInt(jedis.get("balance"));
+        if (balance < amtToSubtract) {
+            jedis.unwatch();
+            System.out.println("modify");
+            return false;
+        } else {
+            System.out.println("***********transaction");
+            Transaction transaction = jedis.multi();
+            transaction.decrBy("balance", amtToSubtract);
+            transaction.incrBy("debt", amtToSubtract);
+            transaction.exec();
+            balance = Integer.parseInt(jedis.get("balance"));
+            debt = Integer.parseInt(jedis.get("debt"));
+
+
+            System.out.println("*******" + balance);
+            System.out.println("*******" + debt);
+            return true;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+       TestTransaction test = new TestTransaction();
+       boolean retValue = test.transMethod();
+       System.out.println("main retValue" + retValue);
     }
 }
